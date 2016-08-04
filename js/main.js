2db20df59553d8218e0e18b62c44d1485c4d23c8
webeffects <nick@alttab.co>
2016-08-04 19:08:17 +0300

    Main

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Mobile menu
+(function(){
+	$('.js-mobile-menu').on('click',function(){
+		var menu = $('.js-mobile-drop') || $(this).next();
+		menu.toggle('fast');
+	});
+})();
+
+// Athlete search
+(function(){
+	$('.js-athlete-search').on('click',function(){
+		$(this).closest('.athlete-search').find('.athlete-search__holder').toggle('fast');
+	});
+	$('.athlete-search__close').on('click',function(){
+		$(this).closest('.athlete-search__holder').toggle('fast');
+	});
+})();
+
+$(function () {
+  $('[data-toggle="tooltip2"]').tooltip({template: '<div class="tooltip tooltip_type_round" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'})
+})
+/*
+(function(){
+	//console.log();
+	$('.athlete-headline .athlete-page__content').affix({
+	  offset: {
+	    top: $('.athlete-headline').position().top,
+	    bottom: function () {
+	      return (this.bottom = $('.footer').outerHeight(true))
+	    }
+	  }
+	});
+})();
+*/
